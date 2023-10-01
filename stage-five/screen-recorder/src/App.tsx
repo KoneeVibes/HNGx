9a704b59afc70114f8fb5e411ede391bb22ef098
Konee <104918313+KoneeVibes@users.noreply.github.com>
2023-10-01 17:41:47 +0100

    there is alot of work to be done here

--- stage-five/screen-recorder/src/App.tsx
+++ stage-five/screen-recorder/src/App.tsx
@@ -2,21 +2,13 @@ import { Popup } from './components/popup';
 import "./App.css";
 import { Context } from './context';
 import { useContext } from 'react';
-import VideoRecorder from 'react-video-recorder';
 
 function App() {
   const { closePopup } = useContext(Context);
   return (
     <>
       {
-        closePopup ?
-          <VideoRecorder
-            onRecordingComplete={(videoBlob: any) => {
-              // Do something with the video...
-              console.log('videoBlob', videoBlob)
-            }}
-          />
-          : <Popup />
+        closePopup ? null : <Popup />
       }
     </>
   );
--- stage-five/screen-recorder/src/components/form.tsx
+++ stage-five/screen-recorder/src/components/form.tsx
@@ -1,12 +1,87 @@
 import { Stack, Button, Typography } from "@mui/material";
 import { SwitchButton } from "./switch";
 import { MicrophoneIcon, VideoCameraIcon } from "../assets";
-import { Context } from "../context";
-import { useContext } from 'react'
+import { useEffect, useState, useRef } from "react";
+
+const mimeType = "video/webm";
 
 export const Form: React.FC<{}> = () => {
+    const mediaRecorder = useRef<MediaRecorder | undefined>(undefined);
+    const liveVideoFeed = useRef(null);
+    const [recordingStatus, setRecordingStatus] = useState("inactive");
+    const [stream, setStream] = useState<MediaStream | undefined>();
+    const [videoChunks, setVideoChunks] = useState([]);
+    const [recordedVideo, setRecordedVideo] = useState<string | null>(null);
 
-    const { setClosePopup } = useContext(Context)
+    const getMicrophonePermission = async () => {
+        setRecordedVideo(null);
+        if ("MediaRecorder" in window) {
+            try {
+                const videoConstraints = {
+                    audio: false,
+                    video: true,
+                };
+                const audioConstraints = { audio: true };
+                // create audio and video streams separately
+                const audioStream = await navigator.mediaDevices.getUserMedia(
+                    audioConstraints
+                );
+                const videoStream = await navigator.mediaDevices.getUserMedia(
+                    videoConstraints
+                );
+
+                //combine both audio and video streams
+                const combinedStream = new MediaStream([
+                    ...videoStream.getVideoTracks(),
+                    ...audioStream.getAudioTracks(),
+                ]);
+                setStream(combinedStream);
+                //set videostream to live feed player
+                if (liveVideoFeed?.current) {
+                    (liveVideoFeed.current as HTMLVideoElement).srcObject = videoStream;
+                    console.log("I work")
+                }
+            } catch (err: any) {
+                alert(err.message);
+            }
+        } else {
+            alert("The MediaRecorder API is not supported in your browser.");
+        }
+    };
+
+    const startRecording = async () => {
+        setRecordingStatus("recording");
+        if (stream) {
+            const media = new MediaRecorder(stream, { mimeType });
+            mediaRecorder.current = media;
+            mediaRecorder.current.start();
+            let localVideoChunks: any = [];
+            mediaRecorder.current.ondataavailable = (event) => {
+                if (typeof event.data === "undefined") return;
+                if (event.data.size === 0) return;
+                localVideoChunks.push(event.data);
+            };
+            setVideoChunks(localVideoChunks);
+        }
+    };
+
+    const stopRecording = () => {
+        setRecordingStatus("inactive");
+        mediaRecorder?.current?.stop();
+        if (mediaRecorder?.current) {
+            mediaRecorder.current.onstop = () => {
+                const videoBlob = new Blob(videoChunks, { type: mimeType });
+                const videoUrl = URL.createObjectURL(videoBlob);
+                setRecordedVideo(videoUrl);
+                setVideoChunks([]);
+            };
+        }
+    };
+
+    useEffect(() => {
+        console.log(stream);
+        console.log(videoChunks);
+    }, [stream, videoChunks]);
 
     return (
         <Stack
@@ -21,7 +96,7 @@ export const Form: React.FC<{}> = () => {
                 Icon={VideoCameraIcon}
             />
             <Button
-                onClick={() => setClosePopup(true)}
+                onClick={getMicrophonePermission}
                 sx={{
                     padding: "16px",
                     borderRadius: "var(--Inner-Radius, 12px)",
@@ -44,6 +119,8 @@ export const Form: React.FC<{}> = () => {
                     Start Recording
                 </Typography>
             </Button>
+            <Button onClick={startRecording}>Start Recording</Button>
+            <Button onClick={stopRecording}>Stop Recording</Button>
         </Stack>
     )
 }
